2efcfc6d31679f21f6232bf5cd52fdd6c41ff97e
unknown <fgxmendoza23@gmail.com>
2021-12-08 18:25:07 +0800

    test print

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -59,7 +59,7 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js"></script>
     <script type="text/javascript" src="/js/bootstrap-datepicker.js"></script>
-    <script src="https://raw.githubusercontent.com/rusty1s/table2excel/master/dist/table2excel.js"></script>
+    <script src="/js/table2excel.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="/vendor/animsition/animsition.min.js"></script>
     <script src="/js/main.js"></script>
